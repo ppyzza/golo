8ff7f0613592ace996543497cdf4d13456d9f07a
Panchanat Parchaya <panchanat.par@pantavanij.com>
2020-08-21 16:46:50 +0700

    [LYNX] progress

---
+++ app/src/main/java/com/hackathon/golo/contract/DetailExplorerContract.java
@@ -0,0 +1,19 @@
+package com.hackathon.golo.contract;
+
+import com.hackathon.golo.model.placedetail.PlaceResponse;
+
+public interface DetailExplorerContract {
+
+    interface View {
+        void showPlaceDetail(PlaceResponse placeResponse);
+    }
+
+    interface Presenter {
+        void getPlaceDetail(String placeId);
+        void dataPlaceDetail(PlaceResponse placeResponse);
+    }
+
+    interface Model {
+        void getPlaceDetail(String placeId);
+    }
+}
---
+++ app/src/main/java/com/hackathon/golo/model/placedetail/Facilities.java
@@ -0,0 +1,12 @@
+package com.hackathon.golo.model.placedetail;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Facilities {
+
+    @SerializedName("code")
+    private String code;
+
+    @SerializedName("description")
+    private String description;
+}
