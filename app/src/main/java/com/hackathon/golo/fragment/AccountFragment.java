c984250fbfb35ec46af69c6ce52229da8e0f57ab
Tananun Tipnee <tananun.tip@pantavanij.com>
2020-08-21 14:19:21 +0700

    [FJ] User - add user info page.

--- app/src/main/java/com/hackathon/golo/fragment/AccountFragment.java
+++ app/src/main/java/com/hackathon/golo/fragment/AccountFragment.java
@@ -1,6 +1,47 @@
 package com.hackathon.golo.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.hackathon.golo.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 public class AccountFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ArrayList<String> data = new ArrayList<>();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        tabLayout = rootView.findViewById(R.id.tab_layout);
+        viewPager = rootView.findViewById(R.id.pager);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        data.add("Plans");
+        data.add("Local Plans");
+        data.add("Reviews");
+
+        for (int i = 0; i < data.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(data.get(i)));
+        }
+
+    }
 }
